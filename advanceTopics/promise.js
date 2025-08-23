cdb277e1b5449b27975572d72d961ca064d4f09d
guddoA <166909151+guddoA@users.noreply.github.com>
2025-08-23 14:21:22 +0000

    Learnt Promise

---
+++ advanceTopics/promise.js
@@ -0,0 +1,89 @@
+const promiseOne = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task is complete")
+        resolve()
+    },1000)
+});
+ 
+promiseOne.then(function(){
+    console.log("Promise consumed"); // after resolve() has been execute then this function will execute.
+})
+
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("Aync task 2");
+        resolve();
+    },1000)
+}).then(function(){
+    console.log("Async 2nd resolved")
+}) 
+
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({username: "Chai", email: "anagoel@geliooo.com"}) // passing data
+    },1000)
+})
+
+promiseThree.then(function(userData){
+    console.log(userData)
+})
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username: "Ananya", password: "samiksha"})
+        } else{
+            reject("Error: something went wrong")
+        }
+    },1000)
+})
+
+promiseFour
+.then((user) => {
+    console.log(user);
+    return user.username
+})
+.then((userName) => {
+    console.log(userName)
+})
+.catch((error) => {
+    console.log(error)
+})
+.finally(() => {
+    console.log("The promise is either resolved or rejected.")
+})
+
+
+
+const promiseFive = new Promise((resolve,reject) => {
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username: "JS", password: "ana@nya666999"})
+        } else{
+            reject("ERROR: there is something wrong")
+        }
+    },1000)
+})
+
+async function consumePromiseFive(){
+    try{
+        const response = await promiseFive
+        console.log(response);
+    } catch(error){
+        console.log(error);
+    }
+}
+consumePromiseFive()
+
+
+async function getAllUsers(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = response.json();
+    console.log("Data: ",data)
+}
+
+getAllUsers()
+
+
